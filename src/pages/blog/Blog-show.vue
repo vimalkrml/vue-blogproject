05865cd56a66a92b54c8fa30501742520552551a
gokul81 <105714204+gokul81@users.noreply.github.com>
2022-07-06 12:25:41 -0700

    style change

--- src/pages/blog/Blog-show.vue
+++ src/pages/blog/Blog-show.vue
@@ -2,12 +2,12 @@
   <custom-container>
     <router-link
       :to="{ name: 'Home' }"
-      class="cursor-pointer text-xs flex gap-2 items-center mb-10"
+      class="cursor-pointer text-xs flex gap-1 items-center mb-10"
     >
       <div class="w-5 md:w-8">
-        <img src="../../assets/left-arrow.png" alt="Back" />
+        <img src="../../assets/left-arrow.png" alt="Back" class="w-[20px]" />
       </div>
-      <h1>Back to Home</h1>
+      <h1 class="text-[#626262]" >Back to Home</h1>
     </router-link>
 
     <no-blogs v-if="isLoading"></no-blogs>
@@ -15,22 +15,13 @@
     <div v-else>
       <img
         :src="blog.url"
-        class="
-          block
-          mb-5
-          h-[300px]
-          md:h-[300px]
-          lg:h-[395px]
-          xl:h-[600px]
-          w-full
-          object-cover
-        "
+        class="block mb-5 h-[300px] md:h-[300px] lg:h-[395px] xl:h-[600px] w-full object-cover"
       />
       <div>
         <h2 class="text-3xl font-primary mb-8 w-full">
           {{ blog.title }}
         </h2>
-        <p class="mb-5 font-primary leading-6 tracking-[0.6px] italic px-6">
+        <p class="mb-5 font-primary leading-6 tracking-[0.6px] italic">
           {{ blog.content }}
         </p>
         <p class="font-primary mb-5 text-md text-[#626262]">
@@ -38,17 +29,7 @@
         </p>
         <img
           :src="blog.image"
-          class="
-            block
-            grayscale
-            mb-5
-            h-[300px]
-            md:h-[300px]
-            lg:h-[395px]
-            xl:h-[600px]
-            w-full
-            object-cover
-          "
+          class="block grayscale mb-5 h-[300px] md:h-[300px] lg:h-[395px] xl:h-[600px] w-full object-cover"
         />
         <p class="font-primary text-md mb-10">{{ blog.conclusion }}</p>
       </div>
