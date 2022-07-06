a4dd9faff962e33298f52d2447463963d6f9ae2e
Vimal KR <vimalkr141@gmail.com>
2022-07-06 22:19:14 +0530

    Loading screen added for index and show

--- src/components/No-blogs.vue
+++ src/components/No-blogs.vue
@@ -1,8 +1,35 @@
 <template>
   <custom-container>
     <div class="header text-center">
-      <h1 class="text-3xl font-bold text-gray-600">Loading</h1>
-      <h2 class="text-xl font-bold text-gray-600 mb-5">Please wait.</h2>
+      <h1 class="text-3xl font-secondary font-bold text-gray-600">Loading</h1>
+      <h2 class="text-xl font-secondary font-bold text-gray-600 mb-5">
+        Please wait.
+      </h2>
+      <div class="flex justify-center gap-2">
+        <div
+          class="
+            bg-gray-600
+            p-1
+            w-2
+            h-5
+            rounded-full
+            animate-bounce
+            blue-circle
+          "
+        ></div>
+        <div
+          class="bg-black p-1 w-2 h-3 rounded-full animate-bounce green-circle"
+        ></div>
+        <div
+          class="bg-gray-600 p-1 w-2 h-5 rounded-full animate-bounce red-circle"
+        ></div>
+        <div
+          class="bg-black p-1 w-2 h-3 rounded-full animate-bounce green-circle"
+        ></div>
+        <div
+          class="bg-gray-600 p-1 w-2 h-5 rounded-full animate-bounce red-circle"
+        ></div>
+      </div>
     </div>
   </custom-container>
 </template>
@@ -28,5 +55,14 @@ export default {
 }
 .hero > .header > p {
   padding: 10px;
+}
+.blue-circle {
+  animation-delay: 0.1s;
+}
+.green-circle {
+  animation-delay: 0.2s;
+}
+.red-circle {
+  animation-delay: 0.3s;
 }
 </style>
--- src/pages/blog/Blog-show.vue
+++ src/pages/blog/Blog-show.vue
@@ -9,34 +9,14 @@
       </div>
       <h1>Back to Home</h1>
     </router-link>
-    <img
-      :src="blog.url"
-      class="
-        block
-        mb-5
-        h-[300px]
-        md:h-[300px]
-        lg:h-[395px]
-        xl:h-[600px]
-        w-full
-        object-cover
-      "
-    />
-    <div>
-      <h2 class="text-3xl font-primary mb-8 w-full">
-        {{ blog.title }}
-      </h2>
-      <p class="mb-5 font-primary leading-6 tracking-[0.6px] italic px-6">
-        {{ blog.content }}
-      </p>
-      <p class="font-primary mb-5 text-md text-[#626262]">
-        {{ blog.description }}
-      </p>
+
+    <no-blogs v-if="isLoading"></no-blogs>
+
+    <div v-else>
       <img
-        :src="blog.image"
+        :src="blog.url"
         class="
           block
-          grayscale
           mb-5
           h-[300px]
           md:h-[300px]
@@ -46,16 +26,43 @@
           object-cover
         "
       />
-      <p class="font-primary text-md mb-10">{{ blog.conclusion }}</p>
+      <div>
+        <h2 class="text-3xl font-primary mb-8 w-full">
+          {{ blog.title }}
+        </h2>
+        <p class="mb-5 font-primary leading-6 tracking-[0.6px] italic px-6">
+          {{ blog.content }}
+        </p>
+        <p class="font-primary mb-5 text-md text-[#626262]">
+          {{ blog.description }}
+        </p>
+        <img
+          :src="blog.image"
+          class="
+            block
+            grayscale
+            mb-5
+            h-[300px]
+            md:h-[300px]
+            lg:h-[395px]
+            xl:h-[600px]
+            w-full
+            object-cover
+          "
+        />
+        <p class="font-primary text-md mb-10">{{ blog.conclusion }}</p>
+      </div>
     </div>
   </custom-container>
 </template>
 
 <script>
+import NoBlogs from "../../components/No-blogs.vue";
 import CustomContainer from "../../components/CustomContainer.vue";
 export default {
   components: {
     CustomContainer,
+    NoBlogs,
   },
   name: "Blog-show",
   props: ["id"],
@@ -63,20 +70,21 @@ export default {
     return {
       blogID: this.id,
       blog: { title: "", content: "", description: "", conclusion: "" },
+      isLoading: true,
     };
   },
   methods: {
     async getBlogs() {
       const res = await fetch("http://localhost:3000/blogs/" + this.blogID);
       const data = await res.json();
-      return JSON.parse(JSON.stringify(data));
+      this.blog = data;
     },
   },
-  async mounted() {
-    let data = await this.getBlogs();
-    // console.log(JSON.parse(JSON.stringify(data)));
-    this.blog = await data;
-    // console.log(JSON.parse(JSON.stringify(this.blog)));
+  mounted() {
+    this.getBlogs();
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 1500);
   },
 };
 </script>
