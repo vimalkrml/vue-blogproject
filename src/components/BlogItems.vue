55820f96739d624f088235e44633d1db97f27b45
Vimal KR <vimalkr141@gmail.com>
2022-07-06 13:44:04 +0530

    vimal

--- src/components/BlogItems.vue
+++ src/components/BlogItems.vue
@@ -1,22 +1,27 @@
 <template>
-  <section class="flex flex-wrap gap-5">
-    <div class="w-[40%] mx-auto mb-5" v-for="blog in blogs" :key="blog.id">
-      <router-link
-        :to="{ name: 'ShowBlogs', params: { id: blog.id } }"
-        class="cursor-pointer text-xs"
-      >
-        <img
-          src="../assets/danie-franco-by0XNgDemsc-unsplash.jpg"
-          class="block max-w-full"
-        />
+  <section>
+    <div class="flex flex-wrap gap-5">
+      <div class="w-[40%] mx-auto mb-5" v-for="blog in blogs" :key="blog.id">
+        <router-link
+          :to="{ name: 'ShowBlogs', params: { id: blog.id } }"
+          class="cursor-pointer text-xs"
+        >
+          <img :src="blog.url" class="block mb-3.5" />
+          <h3 class="font-secondary text-sm opacity-50 mb-3.5 uppercase">
+            Lifestyle
+          </h3>
+          <div v-show="blogs">
+            <h2 class="text-3xl font-primary mb-8">{{ blog.title }}</h2>
+            <p class="text-sm font-primary leading-6 tracking-[0.6px]">
+              {{ blog.content }}
+            </p>
+          </div>
+        </router-link>
+      </div>
+    </div>
 
-        <div v-show="blogs">
-          <h2>{{ blog.title }}</h2>
-          <p class="w-[40%]">
-            {{ blog.content }}
-          </p>
-        </div>
-      </router-link>
+    <div class="text-center mt-10 mb-20">
+      <button class="border-2 border-gray-300 px-4 py-2">Load more</button>
     </div>
   </section>
 </template>
@@ -26,6 +31,7 @@ export default {
   data() {
     return {
       blogs: "",
+      images: [],
     };
   },
   methods: {
--- src/pages/blog/Blog-Home.vue
+++ src/pages/blog/Blog-Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container px-5 mx-auto pt-3">
+  <div class="pt-3">
     <nav class="flex justify-between">
       <h1>MIMINMO</h1>
       <button>Create Blog</button>
@@ -8,17 +8,20 @@
     <div class="mb-10">
       <h2 class="text-center">BLOGS</h2>
     </div>
-    <div>
+    <div class="container px-3">
       <blog-items></blog-items>
     </div>
+    <footer-blog></footer-blog>
   </div>
 </template>
 
 <script>
 import BlogItems from "../../components/BlogItems.vue";
+import FooterBlog from "../../components/Footer-blog.vue";
 export default {
   components: {
     BlogItems,
+    FooterBlog,
   },
 };
 </script>
