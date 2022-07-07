cf170bae944eb81c8de0cc3f768716a229df69f8
Vimal KR <vimalkr141@gmail.com>
2022-07-07 15:36:52 +0530

    footer fixed

--- src/components/Footer-blog.vue
+++ src/components/Footer-blog.vue
@@ -1,26 +1,26 @@
 <template>
-<footer class="" >
-  <div
-    class="sm:flex justify-between bg-gray-100 px-10 md:px-24 py-5 md:py-9 bottom-0"
-  >
-    <div class="flex gap-3 mb-5 sm:mb-0 justify-center sm:justify-start">
-      <a class="font-primary text-sm" href="">Terms and conditions</a>
-      <p class="font-primary text-sm">Privacy</p>
+  <footer>
+    <div
+      class="sm:flex justify-between bg-gray-100 px-10 md:px-24 py-5 lg:py-9"
+    >
+      <div class="flex gap-3 mb-5 sm:mb-0 justify-center sm:justify-start">
+        <a class="font-primary text-sm" href="">Terms and conditions</a>
+        <p class="font-primary text-sm">Privacy</p>
+      </div>
+      <div class="flex gap-4 justify-center sm:justify-start">
+        <h1 class="font-primary text-sm">Follow</h1>
+        <div class="w-5">
+          <img src="../assets/facebook.png" alt="" />
+        </div>
+        <div class="w-5">
+          <img src="../assets/twitter.png" alt="" />
+        </div>
+        <div class="w-5">
+          <img src="../assets/instagram.png" alt="" />
+        </div>
+      </div>
     </div>
-    <div class="flex gap-4 justify-center sm:justify-start">
-      <h1 class="font-primary text-sm">Follow</h1>
-      <div class="w-5">
-        <img src="../assets/facebook.png" alt="" />
-      </div>
-      <div class="w-5">
-        <img src="../assets/twitter.png" alt="" />
-      </div>
-      <div class="w-5">
-        <img src="../assets/instagram.png" alt="" />
-      </div>
-    </div>
-  </div>
-</footer>
+  </footer>
 </template>
 
 <script>
